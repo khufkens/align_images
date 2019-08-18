909b0e5c04fbe958de55b00d4b90d527ba79e19c
khufkens <koen.hufkens@gmail.com>
2019-08-18 15:34:00 +0200

    code

---
+++ README.md
@@ -0,0 +1,64 @@
+## Introduction
+
+This is a small python script showing three methods to align images using OpenCV or standard python based code. Personally I've used all three techniques in various projects, in particular to align images taken by separate raspberry pi with different spectral responses.
+
+You can grab the example code as well as two example images from this repo by cloning it using:
+
+```bash
+git clone image registration
+```
+
+A worked example with the included images is given below.
+
+## Methods
+
+The script includes three different methods for calculating image registration / alignment transformation information.
+
+- FFT phase correlation
+- Enhanced Correlation Coefficient (ECC) maximization
+- Feature based registration
+
+In this list, phase correlation is the simplest method and only calculates the translation (shift) of one image compared to the other. In contrast, the ECC methodology can compensate for both shifts, shifts + rotations (euclidean), shifts + rotation + shear (affine), or [homographic](https://en.wikipedia.org/wiki/Homography_(computer_vision)) (3D) transformations of one image to the next. The feature based methodology uses a homographic representation but uses features within the image rather than a correlation based method on the whole image to search for these values.
+
+Depending on the question at hand selecting the methodolgy which most closely matches the deformation between two images will yield the best (and fastest) results. For example, in a past project images were offset along the x axis only. In this case, a phase correlation method was preferred.
+
+## Worked example
+
+To show the functions of the script I've included two example images, `rgb_image.jpg` and `nir_image.jpg`. These two images were taken using a raspberry pi and two seperate cameras hooked up to a multiplexer to acquire them in rapid succession. The RGB image is a standard colour image, while the other image is a near-infrared (NIR) image taken with raspberry pi NO-IR camera combined with a red-edge filter. Both cameras were physically offset. To align both of the images and fuse the data a correction needs to be made.
+
+|  RGB | NIR  | 
+|---|---|
+| ![](rgb_image.jpg)  | ![](nir_image.jpg)  |
+
+The default settings on the script will call the feature based alignment method and align both images. The aligned image (image 1) will be written to disk as a new image, `reg_image.jpg`.
+
+```bash
+./align_images.py -im1 rgb_image.jpg -im2 nir_image.jpg -m feature
+```
+
+This routine will calculate and output an aligned image and print the transformation matrix to the console.
+
+```bash
+[[ 9.28381660e-01  2.04421105e-02  7.46501803e+01]
+ [-3.16804178e-02  9.76211344e-01 -3.10401493e+00]
+ [-6.64593229e-05  2.67611823e-05  1.00000000e+00]]
+
+```
+
+|  RGB | NIR  | 
+|---|---|
+| ![](reg_image.jpg)  | ![](nir_image.jpg)  |
+
+In this example the final goal was to combine the spectral information of both images into a [vegetation index](https://en.wikipedia.org/wiki/Vegetation_Index). The below image combines the red band of the aligned RGB image with the (red) NIR band into an [NDVI index](https://en.wikipedia.org/wiki/Normalized_difference_vegetation_index). The red colours below show high vegetation densities, corresponding to the trees and grass patches in the image (or their reflections).
+
+![](ndvi.jpg)
+
+## Other settings
+
+The above example uses the feature based method. However, in this example the ECC based methodology worked less well. When using this code you will have to play with the parameters and various methodologies to come to an optimal solution.
+
+For a full list of options use:
+
+```bash
+./align_images.py -h
+```
---
+++ align_images.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+
+# Import necessary libraries.
+import os, argparse, warnings
+import cv2
+import numpy as np
+from numpy.fft import fft2, ifft2, fftshift
+
+# set TF log level (suppress verbose output)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# ignore python warnings
+warnings.simplefilter("ignore")
+
+# argument parser
+def getArgs():
+
+   parser = argparse.ArgumentParser(
+    description = '''Demo script showing various image alignment methods
+                     including, phase correlation, feature based matching
+                     and whole image based optimization.''',
+    epilog = '''post bug reports to the github repository''')
+    
+   parser.add_argument('-im1',
+                       '--image_1',
+                       help = 'image to reference',
+                       required = True)
+                       
+   parser.add_argument('-im2',
+                       '--image_2',
+                       help = 'image to match',
+                       required = True)
+
+   parser.add_argument('-m',
+                       '--mode',
+                       help = 'feature or transform based',
+                       default = 'feature')
+
+   parser.add_argument('-mf',
+                       '--max_features',
+                       help = 'maximum number of features to consider',
+                       default = 5000)
+
+   parser.add_argument('-fr',
+                       '--feature_retention',
+                       help = 'fraction of features to retain',
+                       default = 0.15)
+
+   parser.add_argument('-i',
+                       '--iterations',
+                       help = 'number of ecc iterations',
+                       default = 5000)
+
+   parser.add_argument('-te',
+                       '--termination_eps',
+                       help = 'ecc termination value',
+                       default = 1e-8)
+
+   return parser.parse_args()
+
+# Enhanced Correlation Coefficient (ECC) Maximization
+def eccAlign(im1,im2):
+
+    # Convert images to grayscale
+    im1_gray = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+    im2_gray = cv2.cvtColor(im2,cv2.COLOR_BGR2GRAY)
+
+    # Find size of image1
+    sz = im1.shape
+
+    # Define the motion model
+    warp_mode = cv2.MOTION_AFFINE
+
+    # Define 2x3 or 3x3 matrices and initialize the matrix to identity
+    if warp_mode == cv2.MOTION_HOMOGRAPHY :
+        warp_matrix = np.eye(3, 3, dtype=np.float32)
+    else :
+        warp_matrix = np.eye(2, 3, dtype=np.float32)
+
+    # Define termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
+     number_of_iterations,  termination_eps)
+
+    # Run the ECC algorithm. The results are stored in warp_matrix.
+    (cc, warp_matrix) = cv2.findTransformECC (im1_gray,im2_gray,warp_matrix, warp_mode, criteria)
+
+    if warp_mode == cv2.MOTION_HOMOGRAPHY :
+        # Use warpPerspective for Homography 
+        im2_aligned = cv2.warpPerspective (im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    else :
+        # Use warpAffine for Translation, Euclidean and Affine
+        im2_aligned = cv2.warpAffine(im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
+
+    return im2_aligned, warp_matrix
+
+# (ORB) feature based alignment      
+def featureAlign(im1, im2):
+  
+  # Convert images to grayscale
+  im1Gray = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+  im2Gray = cv2.cvtColor(im2,cv2.COLOR_BGR2GRAY)
+    
+  # Detect ORB features and compute descriptors.
+  orb = cv2.ORB_create(max_features)
+  keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+  keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+  
+  # Match features.
+  matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+  matches = matcher.match(descriptors1, descriptors2, None)
+  
+  # Sort matches by score
+  matches.sort(key=lambda x: x.distance, reverse=False)
+
+  # Remove not so good matches
+  numGoodMatches = int(len(matches) * feature_retention)
+  matches = matches[:numGoodMatches]
+
+  # Draw top matches
+  imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
+  #cv2.imwrite("matches.jpg", imMatches)
+  
+  # Extract location of good matches
+  points1 = np.zeros((len(matches), 2), dtype=np.float32)
+  points2 = np.zeros((len(matches), 2), dtype=np.float32)
+
+  for i, match in enumerate(matches):
+    points1[i, :] = keypoints1[match.queryIdx].pt
+    points2[i, :] = keypoints2[match.trainIdx].pt
+  
+  # Find homography
+  h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+
+  # Use homography
+  height, width, channels = im2.shape
+  im1Reg = cv2.warpPerspective(im1, h, (width, height))
+  
+  return im1Reg, h      
+
+# FFT phase correlation
+def translation(im0, im1):
+    
+    # Convert images to grayscale
+    im0 = cv2.cvtColor(im0,cv2.COLOR_BGR2GRAY)
+    im1 = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+    
+    shape = im0.shape
+    f0 = fft2(im0)
+    f1 = fft2(im1)
+    ir = abs(ifft2((f0 * f1.conjugate()) / (abs(f0) * abs(f1))))
+    t0, t1 = np.unravel_index(np.argmax(ir), shape)
+    if t0 > shape[0] // 2:
+        t0 -= shape[0]
+    if t1 > shape[1] // 2:
+        t1 -= shape[1]
+    return [t0, t1]
+      
+if __name__ == '__main__':
+    
+  # parse arguments
+  args = getArgs()
+    
+  # defaults feature values
+  max_features = args.max_features
+  feature_retention = args.feature_retention
+  
+  # Specify the ECC number of iterations.
+  number_of_iterations = args.iterations
+
+  # Specify the ECC threshold of the increment
+  # in the correlation coefficient between two iterations
+  termination_eps = args.termination_eps
+
+  # Read the images to be aligned
+  im1 =  cv2.imread(args.image_1);
+  im2 =  cv2.imread(args.image_2);
+
+  # Switch between alignment modes
+  if args.mode == "feature":
+   # align and write to disk
+   aligned, warp_matrix = featureAlign(im1, im2)
+   cv2.imwrite("reg_image.jpg",
+    aligned,
+    [cv2.IMWRITE_JPEG_QUALITY, 90])
+   print(warp_matrix)
+  elif args.mode == "ecc":
+   aligned, warp_matrix = eccAlign(im1, im2)
+   cv2.imwrite("reg_image.jpg",
+    aligned,
+    [cv2.IMWRITE_JPEG_QUALITY, 90])
+   print(warp_matrix)
+  else:
+   warp_matrix = translation(im1, im2)
+   print(warp_matrix)
